cedf99e9b251c155959dd304066fd6cd0e25b68b
izaowl <izasow@yahoo.com>
2022-01-11 18:33:53 +0000

    Step 10

--- README.md
+++ README.md
@@ -53,3 +53,7 @@ CREATE DATABASE bookmark_manager;
 2. Create the database using the psql command CREATE DATABASE bookmark_manager;
 3. Connect to the database using the pqsl command \c bookmark_manager;
 4. Run the query we have saved in the file 01_create_bookmarks_table.sql
+
+In order to create testing database execute the following commands:
+CREATE DATABASE "bookmark_manager_test";
+CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # Set the environment to "test"
+
 ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -8,7 +10,13 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
 
